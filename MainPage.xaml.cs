f9ee218cfdf4cb1015a65a2c754df1514db599aa
Usuario <Usuario@CPD03-M15>
2024-05-15 14:39:38 -0300

    update first screen

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,6 +9,15 @@
             InitializeComponent();
         }
 
+        private void btn_criarconta_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Login_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
